172ce364aa4b335c71152fd73456c4e6f6ab1423
Eliott Ham <eliotthham@gmail.com>
2017-07-22 18:07:57 -0700

    Add json format to controller for index and show actions.

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -2,6 +2,10 @@ class KittensController < ApplicationController
 
 	def index
 		@kittens = Kitten.all
+		respond_to do |format|
+			format.html
+			format.json { render json: @kittens }
+		end
 	end
 
 	def new
@@ -10,6 +14,10 @@ class KittensController < ApplicationController
 
 	def show
 		@kitten = Kitten.find(params[:id])
+		respond_to do |format|
+			format.html
+			format.json { render json: @kitten }
+		end
 	end
 
 	def edit
